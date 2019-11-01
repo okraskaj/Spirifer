5c366c2bd763a403cfd022fe915270bafd51fd3c
Jan Okraska <jan.okraska@cern.ch>
2019-11-01 17:49:02 +0100

    models: rename fields in posts model

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=150)
-    post_text = models.CharField(max_length=3000)
+    title = models.CharField(max_length=150)
+    text = models.CharField(max_length=3000)
     pub_date = models.DateTimeField('date published')
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
